00715bce4e634d216f9acf2da9b6f6bc861b1b14
kenillakhani <kenil.rce21@sot.pdpu.ac.in>
2024-09-11 22:49:59 +0530

    Final Application

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             Dive into the World of Anonymous Feedback
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
-            True Feedback - Where your identity remains a secret.
+          MysticTalk - Where your identity remains a secret.
           </p>
         </section>
 
@@ -59,7 +59,7 @@ export default function Home() {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2024 True Feedback. All rights reserved.
+        © 2024 MysticTalk. All rights reserved.
       </footer>
     </>
   );
